43a2e15a85e420408b8d792690ffe25b5838f4cf
FLAVIO SANTOS DE ANDRADE <flavio.ssiv@gmail.com>
2020-01-26 01:21:52 -0300

    proxy setup [...]

--- index.js
+++ index.js
@@ -18,16 +18,20 @@ app.use(bodyParser.json());
 //route for request
 app.get("/api", async (req, res) => {
   // const { target, searchvalue, page } = req.query;
-  // const response = await axios.get(
-  //   `https://ws.audioscrobbler.com/2.0/?method=${target}.search&${target}=${searchvalue}&api_key=${process.env.API_KEY}&page=${page}&format=json`,
-  //   {
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json"
-  //     }
-  //   }
-  // );
-  res.send("Here!", req);
+  try {
+    // const response = await axios.get(
+    //   `https://ws.audioscrobbler.com/2.0/?method=${target}.search&${target}=${searchvalue}&api_key=${process.env.API_KEY}&page=${page}&format=json`,
+    //   {
+    //     headers: {
+    //       Accept: "application/json",
+    //       "Content-Type": "application/json"
+    //     }
+    //   }
+    // );
+    res.send("Here!");
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 //deploy the build version for production
